32a210de328d76d74f95dcfb3a1154b6fd3d6cec
Gennady Petukhov <g3pmail@gmail.com>
2021-06-12 16:02:11 +0300

    Add VideoUseCase

---
+++ app/src/main/java/com/gpetuhov/android/videotestapp/domain/model/VideoInfo.kt
@@ -0,0 +1,3 @@
+package com.gpetuhov.android.videotestapp.domain.model
+
+data class VideoInfo(var url: String)
---
+++ app/src/main/java/com/gpetuhov/android/videotestapp/domain/usecase/VideoUseCase.kt
@@ -0,0 +1,11 @@
+package com.gpetuhov.android.videotestapp.domain.usecase
+
+import com.gpetuhov.android.videotestapp.domain.model.VideoInfo
+
+class VideoUseCase {
+
+    fun getVideoList(): List<VideoInfo> {
+        // TODO
+        return emptyList()
+    }
+}
