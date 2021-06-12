2a6532fab13c00dc9aa486f05221dcab42f38005
Gennady Petukhov <g3pmail@gmail.com>
2021-06-12 20:23:00 +0300

    Add ExoPlayer

--- app/src/main/java/com/gpetuhov/android/videotestapp/presentation/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/videotestapp/presentation/MainActivity.kt
@@ -2,18 +2,25 @@ package com.gpetuhov.android.videotestapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.exoplayer2.SimpleExoPlayer
 import com.gpetuhov.android.videotestapp.App
 import com.gpetuhov.android.videotestapp.R
 import com.gpetuhov.android.videotestapp.domain.usecase.VideoUseCase
 import com.gpetuhov.android.videotestapp.utils.Logger
+import com.gpetuhov.android.videotestapp.utils.extensions.create
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     // TODO: remove this
     @Inject lateinit var videoUseCase: VideoUseCase
+
+    private var player: SimpleExoPlayer? = null
 
     init {
         App.appComponent.inject(this)
@@ -23,10 +30,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO: remove this
+        // TODO: move this into ViewModel
         GlobalScope.launch {
             val videoList = videoUseCase.getVideoList()
             Logger.log("Video",videoList.toString())
+
+            withContext(Dispatchers.Main) {
+                player = player_view.create(
+                    url = videoList[0].url,
+                    isLoop = true
+                )
+            }
         }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        player?.release()
+        player = null
     }
 }
